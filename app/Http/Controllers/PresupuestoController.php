e9ca4bc9d9bb56e1db6335e0ad744ed28dac8462
tocino155 <101911354+tocino155@users.noreply.github.com>
2022-08-13 16:04:21 -0500

    presupuesto_pdf

--- app/Http/Controllers/PresupuestoController.php
+++ app/Http/Controllers/PresupuestoController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use DB;
 use DateTime;
-
+use PDF;
 class PresupuestoController extends Controller
 {
     public function __construct()
@@ -15,25 +15,32 @@ class PresupuestoController extends Controller
     }
     
     public function vista_presupuesto(){
+
         $proyectos=DB::table("proyectos")->where('id',Auth::user()->id_proyecto_select)->first();
-        $presupuestos=DB::table('presupuestos')->select('*')->get();
-        $modulos=DB::table("modulos")->where('id_proyecto',Auth::user()->id_proyecto_select)->get();
+
+        if($proyectos==null){
+            $id_proyecto=0;
+        }else{
+            $id_proyecto=$proyectos->id;
+        }
+        $modulos=DB::table("modulos")->where('id_proyecto',$id_proyecto)->get();
+        $presupuestos=DB::table('presupuestos')->where('id_proyecto',$id_proyecto)->get();
         $sprints=DB::table("sprints")->select("*")->get();
-        $dinamico= DB::table('dinamico_egresos')->select("*")->get();
+        $egresos= DB::table('dinamico_egresos')->select("*")->get();
 
         //sacar semanas de una fecha a otra
         if(isset($proyectos)){
-        $fecha1 = new DateTime($proyectos->fecha);
-        $fecha2 = new DateTime($proyectos->entrega);
-        $semanas= $fecha1->diff($fecha2);
-        $calcular=floor($semanas->format('%a') / 7);
-       
-        return view('Presupuestos.Presupuesto',compact('proyectos','presupuestos','modulos','sprints','calcular'));
+            $fecha1 = new DateTime($proyectos->fecha);
+            $fecha2 = new DateTime($proyectos->entrega);
+            $semanas= $fecha1->diff($fecha2);
+            $calcular=floor($semanas->format('%a') / 7);
+           
+            return view('Presupuestos.Presupuesto',compact('proyectos','presupuestos','modulos','sprints','calcular','egresos'));
 
-    }else{
+        }else{
 
-        return view('Presupuestos.Presupuesto',compact('proyectos','presupuestos','modulos','sprints'));
-    }
+            return view('Presupuestos.Presupuesto',compact('proyectos','presupuestos','modulos','sprints'));
+        }
     
         
     } 
@@ -78,5 +85,73 @@ class PresupuestoController extends Controller
                 
           
         return redirect()->back()->with(['message' => 'Presupuesto Guardado con Éxito', 'color' => 'success']);
+     }
+
+     public function actualizar_presupuesto(Request $request){
+        $request->validate([
+            'costo'=>'required',
+            'integrantes'=>'required',
+            'semanas' =>'required',
+            'sprints' =>'required',
+            'egreso' =>'required',
+            'concepto' =>'required',
+            ]);
+            
+            DB::table("presupuestos")->where("id",$request['id_presupuesto'])->update([
+                "costo"=>$request['costo'],
+                "integrantes"=>$request['integrantes'],
+                "semanas"=>$request['semanas'],
+                "sprints"=>$request['sprints'],
+                "egreso"=>$request['egreso'],
+                "concepto"=>$request['concepto'],
+            ]);
+
+            DB::table("dinamico_egresos")->where("id_presupuesto",$request['id_presupuesto'])->delete();
+
+            if(isset($request['egresos'])){
+                for($i=0;$i<count($request['egresos']);$i++){
+                
+                DB::table("dinamico_egresos")->insert([
+
+                    "id_presupuesto"=>$request['id_presupuesto'],
+                    "egreso"=>$request['egresos'][$i],
+                    "concepto"=>$request['conceptos'][$i],
+                ]);
+            }}
+                
+          
+        return redirect()->back()->with(['message' => 'Presupuesto Actualizado con Éxito', 'color' => 'warning']);
+     }
+
+     public function pdf_presupusto(){
+
+        $proyectos=DB::table("proyectos")->where('id',Auth::user()->id_proyecto_select)->first();
+
+        if($proyectos==null){
+            $id_proyecto=0;
+        }else{
+            $id_proyecto=$proyectos->id;
+        }
+        $modulos=DB::table("modulos")->where('id_proyecto',$id_proyecto)->get();
+        $presupuestos=DB::table('presupuestos')->where('id_proyecto',$id_proyecto)->get();
+        $sprints=DB::table("sprints")->select("*")->get();
+        $egresos= DB::table('dinamico_egresos')->select("*")->get();
+
+        //sacar semanas de una fecha a otra
+        if(isset($proyectos)){
+            $fecha1 = new DateTime($proyectos->fecha);
+            $fecha2 = new DateTime($proyectos->entrega);
+            $semanas= $fecha1->diff($fecha2);
+            $calcular=floor($semanas->format('%a') / 7);
+
+            $pdf = PDF::loadView('Presupuestos.Presupuesto_PDF',compact('proyectos','presupuestos','modulos','sprints','calcular','egresos'))->setPaper(array(0,0,1188,1536),"landscape");
+            $nombre_pdf="Presupuesto_".$proyectos->nombre.".pdf";
+            return $pdf->stream($nombre_pdf);
+
+        }else{
+
+            return view('Presupuestos.Presupuesto_PDF',compact('proyectos','presupuestos','modulos','sprints'));
+        }
+
      }
 }
